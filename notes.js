ef9d60d29073c684d84f6c6889cf80e46e348552
Gavin Lock <gavin@gavlock.dev>
2020-09-14 21:28:42 +0200

    Finished inital work on drawing notes on staves.
    - Fixed bug & simplified code for `Note.staffIndex` calculation
    
    - Started adding SMuFL glyph dictionary
    
    - Implemented `StaffCanvas` component to render staves to an HTML canvas.

--- notes.js
+++ notes.js
@@ -1,49 +1,36 @@
 import Music from './lib/music.mjs';
+import StaffCanvas from './lib/staffCanvas.mjs';
 
 const dbg = window.dbg = {};
 dbg.Music = Music;
 
 $( () => {
-	const notesToPractice = ['C4', 'G4', 'C5', 'F5'];
+	const notesToPractice = ['C3', 'C4', 'G4', 'C5', 'F5'];
 
 	function start() {
 		const selectedNoteIndex = Math.floor(Math.random() * notesToPractice.length);
 		const selectedNote = new Music.Note(notesToPractice[selectedNoteIndex]);
 		console.log(selectedNote);
 
-		const staff = (selectedNote >= Music.Note.middleC) ? new Music.TrebleStaff() : new Music.BassStaff();
-		dbg.staff = staff;
+		//const staff = (selectedNote >= Music.Note.middleC) ? new Music.TrebleStaff() : new Music.BassStaff();
 		//staff.addNote(selectedNote);
 
-		const gClef = '\uE050';
-		const staff5Lines = '\uE014';
-		const noteQuarterUp = '\uE1D5';
-		const noteQuarterDown = '\uE1D6';
+		const staff = new Music.TrebleStaff();
+		dbg.staff = staff;
 
-		const canvas = $('#requestedNote')[0];
-		const context = canvas.getContext('2d');
+		staff.addNote('E4');
+		staff.addNote('G4');
+		staff.addNote('B4');
+		staff.addNote('D5');
+		staff.addNote('F5');
 
-		const fontSize = canvas.height / 2;
-		const staffSpace = fontSize / 4;
-		const margin = fontSize / 4;
-		const staffLineThickness = 0.13;
-		
-		context.font = fontSize + 'px Bravura';
-		const baseLine = canvas.height - 2 * staffSpace;
+		staff.addNote('F4');
+		staff.addNote('A♭4');
+		staff.addNote('C♮5');
+		staff.addNote('E#5');
 
-		context.beginPath();
-		context.lineWidth = staffLineThickness * staffSpace;
-		for (let i = 0 ; i < 5 ; ++i) {
-			//context.fillText(staff5Lines, (margin / 2) + (i * (fontSize / 2 - 1)), baseLine);
-			const yPos = baseLine - (i * staffSpace);
-			context.moveTo((margin / 2), yPos);
-			context.lineTo(canvas.width - (margin / 2), yPos);
-		}
-		context.stroke();
-		
-		context.fillText(gClef, margin, baseLine - staffSpace);
-		context.fillText(noteQuarterUp, margin + 1 * fontSize, baseLine - (-1) * staffSpace);
-		context.fillText(noteQuarterDown, margin + 2 * fontSize, baseLine - 5 * staffSpace);
+		const staffCanvas = new StaffCanvas($('#requestedNote')[0]);
+		staffCanvas.render(staff);
 	}
 
 	function init() {
@@ -55,5 +42,5 @@ $( () => {
 	}
 
 	document.fonts.load('40pt Bravura').then(init);
-	
+
 });
