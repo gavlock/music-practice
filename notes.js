a0de832a4c44ef4d8cb8038cdc6e7221b973e666
Gavin Lock <gavin@gavlock.dev>
2020-09-14 04:36:54 +0200

    Replace staff line glyphs with drawn paths.
    According to the SMuFL implementation notes:
    
    > "Scoring programs should draw their own staff lines using
    > primitives, not use the glyphs ..."

--- notes.js
+++ notes.js
@@ -17,20 +17,26 @@ $( () => {
 		const context = canvas.getContext('2d');
 
 		const fontSize = canvas.height / 2;
-		const staffLineHeight = fontSize / 4;
+		const staffSpace = fontSize / 4;
 		const margin = fontSize / 4;
+		const staffLineThickness = 0.13;
 		
 		context.font = fontSize + 'px Bravura';
-		const baseLine = canvas.height - 2 * staffLineHeight;
+		const baseLine = canvas.height - 2 * staffSpace;
 
-		//TODO: "Scoring programs should draw their own staff lines using
-		//primitives, not use the glyphs in this range"
-		for (let i = 0 ; i < 8 ; ++i)
-			context.fillText(staff5Lines, (margin / 2) + (i * (fontSize / 2 - 1)), baseLine);
+		context.beginPath();
+		context.lineWidth = staffLineThickness * staffSpace;
+		for (let i = 0 ; i < 5 ; ++i) {
+			//context.fillText(staff5Lines, (margin / 2) + (i * (fontSize / 2 - 1)), baseLine);
+			const yPos = baseLine - (i * staffSpace);
+			context.moveTo((margin / 2), yPos);
+			context.lineTo(canvas.width - (margin / 2), yPos);
+		}
+		context.stroke();
 		
-		context.fillText(gClef, margin, baseLine - staffLineHeight);
-		context.fillText(noteQuarterUp, margin + 1 * fontSize, baseLine - (-1) * staffLineHeight);
-		context.fillText(noteQuarterDown, margin + 2 * fontSize, baseLine - 5 * staffLineHeight);
+		context.fillText(gClef, margin, baseLine - staffSpace);
+		context.fillText(noteQuarterUp, margin + 1 * fontSize, baseLine - (-1) * staffSpace);
+		context.fillText(noteQuarterDown, margin + 2 * fontSize, baseLine - 5 * staffSpace);
 	}
 
 	function init() {
